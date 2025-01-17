5dda21382b296a8bc69152c44cbae0292855f7e0
Artyom-exe <thlambot@hotmail.fr>
2025-01-17 15:12:50 +0100

    Ajout de la fonctionnalité de filtrage des conversations et ajustement dynamique de la hauteur de la zone de texte.

--- resources/js/Components/ChatMessage.vue
+++ resources/js/Components/ChatMessage.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 
 
 <template>
-  <div class="flex mb-4">
+  <div class="flex mb-4 w-3/6">
     <!-- Message utilisateur : Aligné à droite -->
     <div v-if="message.role === 'user'" class="bg-gray-700 text-white px-4 py-2 rounded-lg ml-auto"
       aria-label="Message utilisateur">
@@ -24,7 +24,7 @@ const props = defineProps({
 
     <!-- Message IA : Aligné à gauche et rendu en Markdown avec surlignage syntaxique -->
     <div v-else-if="message.role === 'assistant'"
-      class="bg-gray-800 text-gray-300 px-4 py-2 rounded-lg mr-auto prose prose-invert" v-html="message.content"
+      class="text-gray-300 px-4 py-2 rounded-lg mr-auto prose prose-invert" v-html="message.content"
       aria-label="Message assistant"></div>
   </div>
 </template>
