0cef635ab5ca01958485367898daa3d3150049ba
Artyom-exe <th.lambot@hotmail.fr>
2025-01-28 15:49:51 +0100

    fix: correction de la vue SVG pour un rendu approprié de l'icône

--- resources/js/Pages/Ask/Index.vue
+++ resources/js/Pages/Ask/Index.vue
@@ -580,7 +580,7 @@ function adjustHeight(event) {
                     class="h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
-                    viewBox="0 24 24"
+                    viewBox="0 0 24 24"
                     :stroke="'#1f2937'"
                   >
                     <path
