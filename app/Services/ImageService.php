2eb5997270e7dd073e93cdf129f3257b21ad8af1
Artyom-exe <thlambot@hotmail.fr>
2025-02-14 11:54:17 +0100

    feat: ajout de la gestion des images dans les messages de conversation

---
+++ app/Services/ImageService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+class ImageService
+{
+  private const MAX_WIDTH = 800;
+  private const JPEG_QUALITY = 85;
+
+  public function optimizeImage($sourcePath): string
+  {
+    [$width, $height, $type] = getimagesize($sourcePath);
+    $ratio = $width / $height;
+    $newWidth = min($width, self::MAX_WIDTH);
+    $newHeight = (int)($newWidth / $ratio);
+
+    $newImage = imagecreatetruecolor($newWidth, $newHeight);
+
+    $sourceImage = match ($type) {
+      IMAGETYPE_JPEG => imagecreatefromjpeg($sourcePath),
+      IMAGETYPE_PNG => imagecreatefrompng($sourcePath),
+      IMAGETYPE_WEBP => imagecreatefromwebp($sourcePath),
+      default => throw new \Exception('Format d\'image non supporté'),
+    };
+
+    imagecopyresampled(
+      $newImage,
+      $sourceImage,
+      0,
+      0,
+      0,
+      0,
+      $newWidth,
+      $newHeight,
+      $width,
+      $height
+    );
+
+    ob_start();
+    imagejpeg($newImage, null, self::JPEG_QUALITY);
+    $imageData = ob_get_clean();
+
+    imagedestroy($sourceImage);
+    imagedestroy($newImage);
+
+    return $imageData;
+  }
+
+  public function getBase64Image(string $binaryData): string
+  {
+    return "data:image/jpeg;base64," . base64_encode($binaryData);
+  }
+}
